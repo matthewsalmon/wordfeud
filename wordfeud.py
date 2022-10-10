0682c8ed6167c543924ec02dd6d4cada382b2523
Matthew Salmon <matthewsalmon@tutanota.de>
2022-10-10 20:46:56 +0100

    yup

--- wordfeud.py
+++ wordfeud.py
@@ -6,3 +6,14 @@ def player_list(lst):
         print(chunks[x].strip())
 
 
+# split the player list from the wloh email so that it can be pasted into a spreadsheet (column)
+def player_list_set(lst):
+
+    chunks = lst.split(',')
+    mylist = []
+
+    for x in range(len(chunks)):
+        name = chunks[x].strip()
+        mylist.append(name)
+
+    return mylist
